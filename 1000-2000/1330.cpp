717b1e5dd2c0afeafe3a61d9213a2206a32086f2
dangdangss <72215059+dd9595@users.noreply.github.com>
2021-11-13 04:57:22 +0900

    2021-11-13

---
+++ 1000-2000/1330.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	cin >> x >> y;
+
+	if (x > y)
+	{
+		cout << ">" << endl;
+	}
+	else if (x < y)
+	{
+		cout << "<" << endl;
+	}
+	else
+	{
+		cout << "==" << endl;
+	}
+}
